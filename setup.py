5907836fc891d21bc736fc8033d77577dff504a0
Eivind Gard Lund <e.g.lund@medisin.uio.no>
2013-08-05 22:19:24 +0200

    switched to setuptools so that 'develop' installation works

--- setup.py
+++ setup.py
@@ -1,4 +1,5 @@
-from distutils.core import setup
+#from distutils.core import setup
+from setuptools import setup
 from distutils.extension import Extension
 from Cython.Distutils import build_ext
 
