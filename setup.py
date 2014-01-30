53013317d0d41f641e77eacd06c8f4110a157a2f
Eivind Gard Lund <gardlund@gmail.com>
2014-01-30 14:46:01 +0100

    updated version number

--- setup.py
+++ setup.py
@@ -27,7 +27,7 @@ except ImportError:
     raise Exception('please install cython first, e.g.: pip install --upgrade cython')
 
 setup(name='edd',
-      version='0.9.1',
+      version='1.0',
       description='Enriched domain detector for ChIP-seq data',
       url='http://github.com/CollasLab/edd',
       author='Eivind G. Lund',
